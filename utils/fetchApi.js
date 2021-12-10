67f4dadebf97ebb98be68a6854e81dd8b6a8018a
Nitin Sharma <83535855+Nitin-Sharma-coder@users.noreply.github.com>
2021-12-10 12:11:43 +0530

    second commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-export const baseUrl = 'https://bayut.p.rapidapi.com';
+export const baseUrl = "https://bayut.p.rapidapi.com";
 
 export const fetchApi = async (url) => {
-  const { data } = await axios.get((url), {
+  const { data } = await axios.get(url, {
     headers: {
-      'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+      "x-rapidapi-host": "bayut.p.rapidapi.com",
+      "x-rapidapi-key": "fb1a4825demsh5f65cbfa643d848p163e52jsnf37f6536cbc5",
     },
   });
-    
+
   return data;
-}
+};
